6ffa589d9101aebabf7bb486d0e9e570fe290472
Nat Puch <nathanynath@yahoo.fr>
2021-03-16 15:23:02 +0100

    Re-arrange folder.

--- build/start.sh
+++ build/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-chown -R www-data /var/www/html/
+chown -R www-data /var/www/
 chown -R www-data /tmp/pdf
 cd /var/www/html/
+ln -s /tmp/pdf pdf
+chmod +x build.sh
 ./build.sh
 service apache2 start
-tail -F eee
+tail -F aa
